ec3d7150cbe182be627491cf0041f14b4697c8ed
agusmena <agusmena60@mail.com>
2025-12-04 10:03:37 -0300

    feat: add layout, sidebar and mobile header

--- pilotx-test/src/routes/AppRouter.tsx
+++ pilotx-test/src/routes/AppRouter.tsx
@@ -1,14 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import PostPage from "../pages/PostPage";
 import PostDetailPage from "../pages/PostDetailPage";
+import Layout from "../components/Layouts/Layout";
 
 export default function AppRouter() {
   return (
-         <BrowserRouter>
+    <BrowserRouter>
       <Routes>
-        <Route path="/" element={<PostPage />} />
-        <Route path="/post/:id" element={<PostDetailPage />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<PostPage />} />
+          <Route path="/post/:id" element={<PostDetailPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
